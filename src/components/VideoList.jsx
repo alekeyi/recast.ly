c5f4c61f1781d0a77db1ad1c381410d17f940e9e
alekeyi <aleksey.ioseliani@gmail.com>
2019-06-11 11:25:28 -0500

    attempted to connect example video to app.js

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,11 +1,24 @@
-var VideoList = () => (
+// import VideoListEntry from "./VideoListEntry";
+
+// var VideoList = () => (
+//   <div className="video-list">
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//   </div>
+// );
+
+var VideoList = (props) => (
+  // return(
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <h5>
+      {props.videos.map( video => 
+      <VideoListEntry video={video} />)}
+    </h5>
   </div>
+  // );
 );
 
 // PropTypes tell other developers what `props` a component expects
